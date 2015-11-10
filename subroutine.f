af98c345818bbbd1f84083129fe0ee3425ce72c9
Ravi Janjam <ravikishorejnjm@gmail.com>
2015-11-10 04:44:49 -0600

    subroutine example F77

---
+++ subroutine.f
@@ -0,0 +1,17 @@
+
+	integer p
+	write (*, *), 'subroutine example for fortran77'
+	write (*, *), 'please enter an integer '
+	read (*, *) p
+
+	print *, 'The value you gave is', p
+	
+	call squareNo(p)
+
+	stop
+	end
+
+	subroutine squareNo(n)
+	n=n**2
+	print *, 'from subroutine, the square of the no is ', n
+	end
