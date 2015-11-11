5e08ec942c9bafdc1fd2ab79972408f2d095002e
Ravi Janjam <ravikishorejnjm@gmail.com>
2015-11-11 07:26:07 -0600

    script to compile all files to executables

--- compileAll.sh
+++ compileAll.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
+
+if [ $# != 1 ];then
+
+	echo "To compile, do the following "
+	echo "============================="
+	echo "./compileAll c"
+	echo "============================="
+
+	echo " "
+	echo "To delete the compiled ones, do the following "
+	echo "============================="
+	echo "./compileAll d"
+	echo "============================="
+
+elif [ $1 == "c" ]; then
 for i in `ls *f`
 	do 
 		echo "Compiling the file" $i
 
 		gfortran-4.9 -o ${i%.f} $i
 
-		echo "You can run by executing ./"${i%.f} 
+		echo "To execute the following run ./"${i%.f} 
 	done
 
+elif [ $1 == "d" ]; then
+for i in `ls *f`
+	do 
+		echo "Removing the compiled file" ${i%.f}
+
+		rm ${i%.f}
+
+	done
+
+fi
